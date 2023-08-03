195b854ce8d59c9bdeac07a98ddb6da895097adc
amsem <aminesaidsel@gmail.com>
2023-08-03 08:46:21 +0100

    understanding packages structur

---
+++ project/go.mod
@@ -0,0 +1,3 @@
+module github.com/amsem/project
+
+go 1.20
---
+++ project/main.go
@@ -0,0 +1,10 @@
+package main
+
+import ("fmt"
+ "github.com/amsem/project/math")
+func main() {
+  xs := []float64{1,2,3,4}
+  avg := math.Average(xs)
+  fmt.Println(avg)
+  fmt.Println("hello again this my first serious project")
+}
